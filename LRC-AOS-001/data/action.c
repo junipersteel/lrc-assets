cdf6408577c1cb4d2b3ca37363985ffe9e246c09
Julio Arteaga Franqui <jarteaga@fastmail.com>
2024-01-16 22:02:39 +0000

    Initial Commit

---
+++ LRC-AOS-001/data/action.c
@@ -0,0 +1,236 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_cookie("userCart=%7B%22userId%22%3A-1%2C%22productsInCart%22%3A%5B%5D%7D; DOMAIN=advantageonlineshopping.com");
+
+	web_add_auto_header("Accept-Language", 
+		"en-US,en;q=0.9");
+
+	web_add_cookie("_ga_56EMNRF2S2=GS1.2.1705442398.1.1.1705442415.43.0.0; DOMAIN=advantageonlineshopping.com");
+
+	web_add_cookie("_ga_56EMNRF2S2=GS1.2.1705442398.1.1.1705442428.30.0.0; DOMAIN=advantageonlineshopping.com");
+
+	web_add_cookie("_ga_56EMNRF2S2=GS1.2.1705442398.1.1.1705442446.12.0.0; DOMAIN=advantageonlineshopping.com");
+
+	web_add_cookie("_ga_56EMNRF2S2=GS1.2.1705442398.1.1.1705442457.1.0.0; DOMAIN=advantageonlineshopping.com");
+
+	web_add_cookie("_ga_56EMNRF2S2=GS1.2.1705442398.1.1.1705442469.53.0.0; DOMAIN=advantageonlineshopping.com");
+
+	web_url("advantageonlineshopping.com", 
+		"URL=https://advantageonlineshopping.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/main.min.js", ENDITEM, 
+		"Url=/services.properties", ENDITEM, 
+		"Url=/css/images/logo.png", ENDITEM, 
+		"Url=/css/images/closeDark.png", ENDITEM, 
+		"Url=/css/fonts/roboto_regular_macroman/Roboto-Regular-webfont.woff", "Referer=https://advantageonlineshopping.com/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_medium_macroman/Roboto-Medium-webfont.woff", "Referer=https://advantageonlineshopping.com/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_light_macroman/Roboto-Light-webfont.woff", "Referer=https://advantageonlineshopping.com/css/main.min.css", ENDITEM, 
+		"Url=/css/images/arrow_right.png", "Referer=https://advantageonlineshopping.com/css/main.min.css", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=tablets", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=headphones", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=mice", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=laptops", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=speakers", ENDITEM, 
+		"Url=/css/images/Special-offer.jpg", ENDITEM, 
+		"Url=/css/fonts/roboto_bold_macroman/Roboto-Bold-webfont.woff", "Referer=https://advantageonlineshopping.com/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_thin_macroman/Roboto-Thin-webfont.woff", "Referer=https://advantageonlineshopping.com/css/main.min.css", ENDITEM, 
+		"Url=/css/images/facebook.png", ENDITEM, 
+		"Url=/css/images/GoUp.png", ENDITEM, 
+		"Url=/css/images/twitter.png", ENDITEM, 
+		"Url=/css/images/linkedin.png", ENDITEM, 
+		"Url=/css/images/Banner2.jpg", ENDITEM, 
+		"Url=/css/images/Banner1.jpg", ENDITEM, 
+		"Url=/css/images/Banner3.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item3.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item2.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item1.jpg", ENDITEM, 
+		"Url=/css/images/category_banner_4.png", ENDITEM, 
+		"Url=/css/images/Filter.png", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4200", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4700", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4600", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4400", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4500", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4100", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4101", ENDITEM, 
+		"Url=/css/images/category_banner_3.png", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=3300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=3200", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=3100", ENDITEM, 
+		"Url=/css/images/category_banner_5.png", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5400", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5505", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5200", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5800", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5600", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5100", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5900", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=5700", ENDITEM, 
+		LAST);
+
+	web_url("ALL", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/DemoAppConfig/parameters/by_tool/ALL", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_custom_request("GetAccountConfigurationRequest", 
+		"URL=https://advantageonlineshopping.com/accountservice/ws/GetAccountConfigurationRequest", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/xml", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		"EncType=text/xml; charset=UTF-8", 
+		"Body=<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><GetAccountConfigurationRequest xmlns=\"com.advantage.online.store.accountservice\"></GetAccountConfigurationRequest></soap:Body></soap:Envelope>", 
+		LAST);
+
+	web_add_cookie("_ga_TBPYED8WSW=GS1.1.1705442398.1.0.1705442398.0.0.0; DOMAIN=advantageonlineshopping.com");
+
+	web_add_cookie("_ga=GA1.2.781668216.1705442398; DOMAIN=advantageonlineshopping.com");
+
+	web_add_cookie("_gid=GA1.2.618178578.1705442398; DOMAIN=advantageonlineshopping.com");
+
+	web_add_cookie("_gat=1; DOMAIN=advantageonlineshopping.com");
+
+	web_url("categories", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/categories", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_cookie("_ga_56EMNRF2S2=GS1.2.1705442398.1.0.1705442398.60.0.0; DOMAIN=advantageonlineshopping.com");
+
+	web_url("search", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/deals/search?dealOfTheDay=true", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("popularProducts.json", 
+		"URL=https://advantageonlineshopping.com/app/tempFiles/popularProducts.json", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("home-page.html", 
+		"URL=https://advantageonlineshopping.com/app/views/home-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_set_sockets_option("TLS_SNI", "0");
+
+	lr_think_time(11);
+
+	lr_start_transaction("trx_Speakers");
+
+	web_url("products", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/categories/4/products", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("attributes", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/categories/attributes", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t9.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("category-page.html", 
+		"URL=https://advantageonlineshopping.com/app/views/category-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t10.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_set_sockets_option("TLS_SNI", "1");
+
+	lr_end_transaction("trx_Speakers",LR_AUTO);
+
+	lr_think_time(16);
+
+	lr_start_transaction("trx_Speakers_Buy_Now");
+
+	web_url("19", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/products/19", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t11.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("products_2", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/categories/4/products", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t12.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("product-page.html", 
+		"URL=https://advantageonlineshopping.com/app/views/product-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t13.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("all_data", 
+		"URL=https://advantageonlineshopping.com/catalog/api/v1/categories/all_data", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://advantageonlineshopping.com/", 
+		"Snapshot=t14.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("trx_Speakers_Buy_Now",LR_AUTO);
+
+	lr_start_transaction("trx_Tablets");
+
+	lr_end_transaction("trx_Tablets",LR_AUTO);
+
+	lr_start_transaction("trx_Mice");
+
+	lr_end_transaction("trx_Mice",LR_AUTO);
+
+	return 0;
+}
